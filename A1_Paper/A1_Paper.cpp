1174bdbeb91403f92f58b716ef2817a7fea3866b
sohrabag <33716625+sohrabag@users.noreply.github.com>
2018-01-13 12:57:03 +0100

    Add project files.

---
+++ A1_Paper/A1_Paper.cpp
@@ -0,0 +1,26 @@
+// A1_Paper.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+
+typedef struct papertag {
+	float l;
+	float w;
+	int c;
+};
+
+
+int main()
+{
+	int n(0);
+	
+	std::cin >> n;
+	
+	std::vector<papertag> sheets;
+	
+	for(int i = 0; i < n; i++){}
+    return 0;
+}
+
